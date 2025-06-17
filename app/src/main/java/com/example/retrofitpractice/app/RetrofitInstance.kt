ae0a421073cc8fbacadceef6f75e845fe3ba744e
Kv-Ramana01 <kv42216@gmail.com>
2025-06-17 14:35:04 +0530

    retrofit instance done.

---
+++ app/src/main/java/com/example/retrofitpractice/app/RetrofitInstance.kt
@@ -0,0 +1,27 @@
+package com.example.retrofitpractice.app
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitInstance {
+    private const val BASE_URL = "https://fakestoreapi.com/"
+
+    private val loggingInterceptor = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    private val httpClient = OkHttpClient.Builder()
+        .addInterceptor(loggingInterceptor)
+        .build()
+
+    val api: FakeStoreApiService by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(httpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(FakeStoreApiService::class.java)
+    }
+}
